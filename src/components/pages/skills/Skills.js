1e1546b0cde13ae5767f85a192afa059f006ef58
dorinserpe <64789850+dorinserpe@users.noreply.github.com>
2021-05-30 12:17:14 +0300

    Created Skills component

---
+++ src/components/pages/skills/Skills.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default function Skills() {
+  return (
+    <div>
+      <div className="card">
+        <div className="card-content">
+          <h6>
+            <strong>PROFESSIONAL SKILLS</strong>
+          </h6>
+          <div className="row mt-top">
+            <div className="col s6">
+              <p>Linux</p>
+            </div>
+            <div className="col s6">
+              <p>C Programming Language </p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s6">
+              <p>Java</p>
+            </div>
+            <div className="col s6">
+              <p>Git</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col s6">
+              <p>Cisco Packet Tracer</p>
+            </div>
+            <div className="col s6">
+              <p>Wireshark</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
